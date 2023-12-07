c4c12a2cccb55364e351c0e0cd929e124fb205c6
madalafk <fmadala@hotmail.com>
2023-12-07 11:58:07 -0800

    3-to_str.py

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -6,7 +6,7 @@ type-annotated function to_str
 
 def to_str(n: float) -> str:
     '''
-     takes a float n as argument and returns the string 
+     takes a float n as argument and returns the string
      representation of the float.
     '''
     return str(n)
