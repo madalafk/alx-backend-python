811cc4bf02d6593adebd7878077dbc354cd9b9ad
madalafk <fmadala@hotmail.com>
2023-12-07 11:07:46 -0800

    1-concat.py

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+annotated function concat
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    returns: two strings concatenated
+    '''
+    return str1 + str2
